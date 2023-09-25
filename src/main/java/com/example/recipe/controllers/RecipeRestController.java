8deb37706ebc4a3826dcce5db02843bee566e148
drvg5 <32469979+drvg5@users.noreply.github.com>
2023-09-25 13:27:44 +0530

    Adding method implementations

--- src/main/java/com/example/recipe/controllers/RecipeRestController.java
+++ src/main/java/com/example/recipe/controllers/RecipeRestController.java
@@ -1,38 +1,45 @@
 package com.example.recipe.controllers;
 
 import com.example.recipe.models.Recipe;
+import com.example.recipe.repositories.RecipeJpaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/recipes")
 public class RecipeRestController {
-    @GetMapping
+
+    @Autowired
+    private RecipeJpaRepo recipeJpaRepo;
+
+    @GetMapping("")
     public List<Recipe> listRecipes(){
-        return null;
+        return recipeJpaRepo.findAll();
     }
 
-    @GetMapping
-    public Recipe getRecipe(@PathVariable Long id){
-
-        return null;
+    @GetMapping("/{id}")
+    public Optional<Recipe> getRecipe(@PathVariable Long id){
+        return recipeJpaRepo.findById(id);
     }
 
     @PostMapping("")
     public Recipe createRecipe(@RequestBody Recipe recipe){
-
-        return null;
+        return recipeJpaRepo.save(recipe);
     }
 
     @PutMapping("/{id}")
     public Recipe updateRecipe(@RequestBody Recipe recipe, @PathVariable Long id){
-
-        return null;
+        Recipe rep = recipeJpaRepo.getById(id);
+        rep.setName(recipe.getName());
+        rep.setDescription(recipe.getDescription());
+        return recipeJpaRepo.save(rep);
     }
 
     @DeleteMapping("/{id}")
     public void deleteRecipe(@PathVariable Long id){
-
+        recipeJpaRepo.deleteById(id);
     }
 }
